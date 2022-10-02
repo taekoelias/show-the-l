57a72cd4b8dee37c683ca183771313397b390966
Taeko Elias <43224089+taekoelias@users.noreply.github.com>
2022-10-02 20:12:09 -0300

    Correção na cor do texto do grafico

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -7,8 +7,8 @@ export const ProgressBar = ({label, percentage}: ProgressBarProps) => {
     return (
         <>
             <div className="flex justify-between mb-1">
-              <span className="text-base font-medium text-blue-700 dark:text-white">{label}</span>
-              <span className="text-sm font-medium text-blue-700 dark:text-white">{percentage}%</span>
+              <span className="text-base font-medium text-blue-700">{label}</span>
+              <span className="text-sm font-medium text-blue-700">{percentage}%</span>
             </div>
             <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
               <div className="bg-blue-600 h-2.5 rounded-full" style={{width: `${percentage.split(',')[0]}%`}}></div>
